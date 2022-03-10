f00adc49f63111ecc9118f9f2e0d75915da41b77
Guilherme <guilhermepassarinho@gmail.com>
2022-03-09 23:52:55 -0300

    project done

---
+++ client/src/components/SectionAboutUs/content.ts
@@ -0,0 +1,14 @@
+const content = [
+  {
+    name: 'Guilherme Passarinho',
+    image: 'guilherme.jpg',
+    description:
+      'Desenvolvedor Front-end apaixonado pelo ecosistema javascript e atualmente comprometido com a melhor empresa do brasil (Goomer)',
+    socialLinks: [
+      { slug: 'twitter', link: 'https://twitter.com/gguip_' },
+      { slug: 'github', link: 'https://github.com/gguip' }
+    ]
+  }
+]
+
+export default content
